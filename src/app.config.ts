63eee1e083738617d87676b30183b6e6fb5436dd
mac <202241054088@jmu.edu.cn>
2025-01-29 20:58:22 +0800

    init: 初始化小红书项目

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#ff2442',
+    navigationBarTitleText: '小红书',
+    navigationBarTextStyle: 'white'
+  }
+}) 
